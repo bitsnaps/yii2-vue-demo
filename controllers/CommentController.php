cc951b059d791b717d4b9dfd5e646711190ec0a6
bitsnaps <bitsnaps@yahoo.fr>
2020-04-19 00:44:02 +0100

    customize CommentController actions to filter posts by id

--- controllers/CommentController.php
+++ controllers/CommentController.php
@@ -4,6 +4,7 @@ namespace app\controllers;
 
 use app\resource\Comment;
 use yii\rest\ActiveController;
+use yii\data\ActiveDataProvider;
 
 /**
 * class ActiveController
@@ -16,5 +17,29 @@ class CommentController extends ActiveController //\yii\web\Controller
     // by default ActiveController respond with JSON & XML formats
     /* Yii2 by default doesn't parse JSON body (add JsonParser to config/web.php[component\request])*/
     public $modelClass = Comment::class;
+
+    /**
+    * Override request actions and write a custom one
+    */
+    public function actions(){
+      $actions = parent::actions();
+
+      // remove the default implementations
+      // unset($actions['index']);
+
+      // override the ActiveDataProvider
+      $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+      return $actions;
+    }
+
+
+    public function prepareDataProvider(){
+      return new ActiveDataProvider([
+        // andWhere() is generally more save than where()
+        'query' => Comment::find()->andWhere([
+          'post_id' => \Yii::$app->request->get('postId')
+        ])
+      ]);
+    }
 
 }
